acd551996e673d939f2f517e1cb9a76338c8f630
Ed-Neema <98660147+Ed-Neema@users.noreply.github.com>
2023-10-05 00:14:43 +0200

    create test route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from "express"
 import {connectDB} from "./utils/dbConnection.js";
-
+import userRoutes from  "./routes/user.route.js"
 
 const app = express();
 const port = process.env.PORT || 5000;
 connectDB();
+app.use(express.json())
 app.listen(port, ()=>{
     console.log("Server Listening on port: ", port)
 })
+
+app.use("/api/user",userRoutes);
