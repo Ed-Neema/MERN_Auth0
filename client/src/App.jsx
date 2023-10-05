dfa32ca81fb63f979d04e71ee4a55da938fe652b
Ed-Neema <98660147+Ed-Neema@users.noreply.github.com>
2023-10-06 00:54:48 +0200

    redux toolkit setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+
 const App = () => {
   return (
     <BrowserRouter>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { signInStart,signInSuccess, signInFailure } from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-   const [error, setError] = useState(false);
-   const [loading, setLoading] = useState(false);
-  // const { loading, error } = useSelector((state) => state.user);
+  //  get state of error and loading
+  const { loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -15,7 +16,7 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // dispatch(signInStart());
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -25,13 +26,14 @@ const SignIn = () => {
       });
       const data = await res.json();
       if (data.success === false) {
-        // dispatch(signInFailure(data));
+        dispatch(signInFailure(data));
         return;
       }
-      // dispatch(signInSuccess(data));
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      // dispatch(signInFailure(error));
+      dispatch(signInFailure(error));
+     
     }
   };
   return (
