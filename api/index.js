ce8f337cc7e2b28c217f7394ea279bd3b7372d54
Ed-Neema <98660147+Ed-Neema@users.noreply.github.com>
2023-10-08 18:43:57 +0200

    pre-production changes

--- api/index.js
+++ api/index.js
@@ -3,8 +3,16 @@ import {connectDB} from "./utils/dbConnection.js";
 import userRoutes from  "./routes/user.route.js"
 import authRoutes from  "./routes/auth.route.js"
 import cookieParser from "cookie-parser";
+import path from "path"
 // import cors from "cors"
 const app = express();
+// find the directory name that our app will run on in our server
+const __dirname = path.resolve() //find the dynamic director name
+
+app.use(express.static(path.join(__dirname,'/client/dist'))) //serve static files from the specified directory
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html")); //for any GET request that doesn't match the static files in the 'client/dist' directory, it will serve the 'index.html' file.
+});
 const port = process.env.PORT || 5000;
 connectDB();
 // app.use(cors())
@@ -16,6 +24,7 @@ app.listen(port, ()=>{
 
 app.use("/api/user",userRoutes);
 app.use("/api/auth",authRoutes);
+
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
